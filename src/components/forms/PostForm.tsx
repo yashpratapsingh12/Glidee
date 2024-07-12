ff878f0f34e2d2002eaf2cffb3d471ed080ec815
yashpratapsingh12 <yashpratapsingh577@gmail.com>
2024-07-12 00:46:45 -0400

    fixing updatepost funtionality

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -61,7 +61,7 @@ const PostForm = ({post,action}:PostFormProps) => {
             imageUrl:post?.imageUrl,
 
           })
-          if(!updatePost){
+          if(!updatedPost){
             toast({title:'Please try again'})
           }
 
