2957f6e8c62475481889c9c1421c8377ed48d60c
yashpratapsingh12 <yashpratapsingh577@gmail.com>
2024-07-12 23:26:08 -0400

    fixing forms

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -65,8 +65,10 @@ const SigninForm = () => {
   return (
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col">
-        <img src="/assets/images/logo.svg" alt="logo" />
+        <div className="ml-16">
 
+        <img src="/assets/images/logo.svg" alt="logo" />
+        </div>
         <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
           Log in to your account
         </h2>
